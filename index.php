ce3da49c5d058ee71e5b5c6ff378e7aae4455894
AtomJump <peter@atomjump.com>
2019-12-19 12:11:43 +1300

    switch to a button

--- index.php
+++ index.php
@@ -18,6 +18,19 @@
             
         }
         
+        public function on_upload_screen($message_id)
+        {
+        	global $root_server_url;
+        	
+            //Do your thing in here. Here is a sample.
+            $api = new cls_plugin_api();
+          
+           
+            $ret_text = "<a href=\"javascript:\" onclick=\"alert('TODO: MedImage Photo Export in here. Msg ID: " . $message_id . "'); \"><img src='" . $root_server_url . "/plugins/medimage_export/medimage_logo.png'></a>";
+            return $ret_text;
+            
+        }
+        
         
     }
 ?>
