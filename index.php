2cf4c89b2fada55db7c4c98771b2027a80bf5210
AtomJump <peter@atomjump.com>
2019-12-19 12:09:08 +1300

    switch to a button

--- index.php
+++ index.php
@@ -7,11 +7,13 @@
         
         public function on_msg_buttons($message_id)
         {
+        	global $root_server_url;
+        	
             //Do your thing in here. Here is a sample.
             $api = new cls_plugin_api();
           
            
-            $ret_text = "<a href=\"javascript:\" onclick=\"alert('TODO: Export in here. Msg ID: " . $message_id . "'); \">MedImage Export</a>";
+            $ret_text = "<a href=\"javascript:\" onclick=\"alert('TODO: MedImage Photo Export in here. Msg ID: " . $message_id . "'); \"><img src='" . $root_server_url . "/plugins/medimage_export/medimage_logo.png'></a>";
             return $ret_text;
             
         }
