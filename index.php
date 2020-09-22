f578f3024aadb65c17bfe51bd271224c429876e3
AtomJump <peter@atomjump.com>
2020-09-22 13:36:15 +1200

    minor tweaks

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
         		}
            	</script> */
             
-           $ret_text = "<script>function medimageExport(msgId) { alert('Clicked'); $.ajax({ url: \"" . trim_trailing_slash_local($root_server_url) . "/plugins/medimage_export/export.php\", data: data, type: 'POST', cache: false 		}).done(function(response) { alert(\"Response : \" + response);	}); }</script><a class=\"comment-msg-button\" href=\"javascript:\" onclick=\"alert('About to run MedImage Photo Export in here. Msg ID: " . $message_id . "'); medimageExport(" . $message_id . ");\"><img width=\"48\" src='" . $root_server_url . "/plugins/medimage_export/medimage_logo.png'></a>";
+           $ret_text = "<script>function medimageExport(msgId) { alert('Clicked'); data = null; jQuery.ajax({ url: \"" . trim_trailing_slash_local($root_server_url) . "/plugins/medimage_export/export.php\", data: data, type: 'POST', cache: false 		}).done(function(response) { alert(\"Response : \" + response);	}); }</script><a class=\"comment-msg-button\" href=\"javascript:\" onclick=\"alert('About to run MedImage Photo Export in here. Msg ID: " . $message_id . "'); medimageExport(" . $message_id . ");\"><img width=\"48\" src='" . $root_server_url . "/plugins/medimage_export/medimage_logo.png'></a>";
             return $ret_text;
             
         }
