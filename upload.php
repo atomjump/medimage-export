3d18d23f129da2162313f31d47cd1b0751dd48be
AtomJump <peter@atomjump.com>
2020-09-24 08:38:21 +1200

    post_data

--- upload.php
+++ upload.php
@@ -104,7 +104,7 @@
  
     
     if(isset($argv[5])) {
-    		$filename = "image.jpg";		//TODO: get incoming name
+    		$filename = "image-mydate.jpg";		//TODO: get incoming name
     		$upload_to = $argv[5];
     		//Split up the medimage-server value e.g. https://medimage-nz1.atomjump.com/write/uPSE4UWHmJ8XqFUqvf
     		error_log("MedImage Server on upload:" . $upload_to);
