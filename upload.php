29a5d951648fd077db681de99f478c005b7b4572
AtomJump <peter@atomjump.com>
2020-09-24 13:23:41 +1200

    post_data

--- upload.php
+++ upload.php
@@ -60,6 +60,22 @@
 			if(!curl_errno($curl))
 			{
 				$info = curl_getinfo($curl);
+		//Debug in here >>>>>>>>>>>>>.
+	$api = new cls_plugin_api();
+	sleep(2);
+	 $new_message = "Sorry there was a problem sending the photo to the MedImage Server: 'image' [TESTING curl response: " . $info['http_code'] . "]"; 
+     $recipient_ip_colon_id = "";		//No recipient, so the whole group. 
+	 $sender_name_str = "MedImage";
+	 $sender_email = "info@medimage.co.nz";
+	 $sender_ip = "111.111.111.111";
+	 $options = array('allow_plugins' => false);
+	 $message_forum_id = 178;
+	 if($verbose == true) error_log("About to post to the group:" . $message_forum_id);
+	 $api->new_message($sender_name_str, $new_message, $recipient_ip_colon_id, $sender_email, $sender_ip, $message_forum_id, $options);
+   	 exit(0);
+    //To here  <<<<<<<<<<<<
+				
+				
 				if ($info['http_code'] == 200) {
 				  // Files uploaded successfully.
 				  $success = true;
