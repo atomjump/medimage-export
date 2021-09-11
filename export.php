5f1d1569360092bbc6f279c6b81a0b8bbd9f77ef
AtomJump <peter@atomjump.com>
2021-09-12 08:19:00 +1200

    sending image

--- export.php
+++ export.php
@@ -43,9 +43,84 @@
 
 
 
-	function send_image()
+	function get_current_id($api, $message_forum_id)
 	{
+		//This query will find the latest on this forum. May need some refinements e.g. around user id
+		//With var_shouted example value:
+		//MedImage: Switched MedImage patient to ID: 'nhi123 arm'
+		
+		$sql = "SELECT * from tbl_ssshout where int_layer_id = " . $message_forum_id . " AND var_shouted like 'MedImage: Switched%' order by int_ssshout_id desc limit 1";
+		$result = $api->db_select($sql);
+		
+					
+		$row = $api->db_fetch_array($result);
+		if($row) {
+			$message = $row['var_shouted'];
+			$between = explode("'", $message);
+			if($between[1]) {
+				return $between[1];
+			
+			}
+		}
+		
+		return false;
 	
+	}
+
+
+	function send_image($api, $message_id, $image_hi_name, $image_folder, $message_forum_id, $medimage_config)
+	{
+		$verbose = true;   //usually false, unless you want to debug
+		
+		$image_folder = $this->add_trailing_slash_local($medimage_config['serverPath']) . "images/im/";
+		
+		
+		//Send a message to the forum
+		$id_text = get_current_id($api, $message_forum_id);
+		if(!$id_text) {
+			$id_text = "image";
+			$append_message = " Note: you can name your photo by entering e.g. 'id nhi1234 arm'";
+		} else {
+			$append_message = "";
+		}
+		
+		$tags = str_replace(" ", "-", $id_text);
+							
+		
+		$new_message = "Sending photo to the MedImage Server: '" . $id_text . "'" . $append_message;		
+		$recipient_ip_colon_id =  "123.123.123.123:" . $sender_id;		//Send privately to the original sender
+		$sender_name_str = "MedImage";
+		$sender_email = "info@medimage.co.nz";
+		$sender_ip = "111.111.111.111";
+		$options = array('notification' => false, 'allow_plugins' => false);
+		$api->new_message($sender_name_str, $new_message, $recipient_ip_colon_id, $sender_email, $sender_ip, $message_forum_id, $options);
+		
+		
+		//Now start a parallel process, that waits until the photo has been sent, before sending a confirmation message.       
+		
+	
+						
+		//Get the layer name, if available. Used to ensure we have selected the correct database in our process child.
+		$layer_name = "";
+		if(isset($_REQUEST['passcode'])) {
+			$layer_name = $_REQUEST['passcode'];			
+		}
+
+		if(isset($_REQUEST['uniqueFeedbackId'])) {
+			$layer_name = $_REQUEST['uniqueFeedbackId'];
+		}
+
+		$command = $medimage_config['phpPath'] . " " . dirname(__FILE__) . "/upload.php " . $image_folder . " " . $image_hi_name . " " . $message_id . " " . $message_forum_id . " " . $layer_name . " " . $_COOKIE['medimage-server'] . " " . $tags;
+		global $staging;
+		if($staging == true) {
+			$command = $command . " staging";   //Ensure this works on a staging server  
+		}
+		if($verbose == true) error_log("Running: " . $command);
+		
+		//TESTING OUT $api->parallel_system_call($command, "linux");
+		echo "Running: " . $command . "\n";		//TESTING					
+												
+		
 	
 	}
    
@@ -56,7 +131,7 @@
 		
 		$image_folder = add_trailing_slash_local($cnf['fileRoot']) . "images/im/";
 	
-		$sql = "SELECT int_ssshout_id, var_shouted FROM tbl_ssshout WHERE int_ssshout_id = " . $message_id;
+		$sql = "SELECT int_ssshout_id, var_shouted, int_layer_id FROM tbl_ssshout WHERE int_ssshout_id = " . $message_id;
 		//echo $sql . "\n";
 		$result_msgs = $api->db_select($sql);
 		while($row_msg = $api->db_fetch_array($result_msgs))
@@ -82,10 +157,13 @@
 					$image_hi_name = $between_slashes[$len] . "_HI.jpg";
 					echo "Image name: " . $image_name . "\n";
 	
+					$message_forum_id = $row_msg['int_layer_id'];
+					$message_id = $row_msg['int_ssshout_id'];
 	
-					//Send this image
-					send_image($image_name, $image_folder);
-					send_image($image_hi_name, $image_folder);
+					//Send this image - TODO check the hi version exists and send that, but otherwise, send the smaller version.
+					
+					//TEMPOUTsend_image($api, $message_id, $image_name, $image_folder, $message_forum_id, $medimage_config);
+					send_image($api, $message_id, $image_hi_name, $image_folder, $message_forum_id, $medimage_config);
 				}
 			}
 		}
