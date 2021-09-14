331542512a9549157757c1c3e04f2c0854838866
AtomJump <peter@atomjump.com>
2021-09-15 08:52:57 +1200

    1st export

--- pdf-export/export.php
+++ pdf-export/export.php
@@ -125,7 +125,7 @@
  		   list($image_url, $image_filename, $image_hi_filename, $abs_image_dir) = parse_for_image($lines->res[$cnt]->text, $web_api_url, $api_file_path);
  		   
  		   
- 		   //$urls = $image_url . "," . $image_filename . "," . $image_hi_filename . "," . $abs_image_dir;		//TEMP DEBUGGING
+ 		   $urls = $image_url . "," . $image_filename . "," . $image_hi_filename . "," . $abs_image_dir;		//TEMP DEBUGGING
  		   
  		   if($image_url != false) {
  		   	  //So, it is at least an image from another website
@@ -172,7 +172,7 @@
  		   
  
 		   $table->easyCell($line_text, 'width:70%; align:L; bgcolor:' . $background_colour . '; valign:T;' . $image_str);
-		   $table->easyCell($timestamp, 'width:30%; align:L; bgcolor:' . $background_colour . '; valign:T;');
+		   $table->easyCell($ago . " " . $urls, 'width:30%; align:L; bgcolor:' . $background_colour . '; valign:T;');
 		   $table->printRow();
  
  	   }
