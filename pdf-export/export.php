3861e97a80f1bb9c5fb6eb1c310d2b111aa8f925
AtomJump <peter@atomjump.com>
2021-09-14 15:01:52 +1200

    1st export

--- pdf-export/export.php
+++ pdf-export/export.php
@@ -1,5 +1,7 @@
 <?php
 
 	echo "Inside export. Layer:" . $_REQUEST['layer_name'];
+	file_get_contents("https://staging.atomjump.com/api/download.php?format=json&uniqueFeedbackId=test_feedback");
+	//Sample URL: https://staging.atomjump.com/api/download.php?format=json&uniqueFeedbackId=test_feedback
 
 ?>
