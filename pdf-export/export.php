9eec1c9da8859d3db4fcfb4e8bdaa94e06e3b9cc
AtomJump <peter@atomjump.com>
2021-09-14 15:13:36 +1200

    1st export

--- pdf-export/export.php
+++ pdf-export/export.php
@@ -69,8 +69,9 @@
  
  	  //echo $json;
  	  $lines = json_decode($json);
+ 	  print_r($lines);
  	  //echo "With arrow: " . $lines['res'][0]->text;
- 	  echo "With array: " . $lines['res'][0]['text'];
+ 	  //echo "With array: " . $lines['res'][0]['text'];
  	  
  
 	} else {
