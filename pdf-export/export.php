ce66654653eb351dfd15d199b4de108520f3ce56
AtomJump <peter@atomjump.com>
2021-09-16 10:18:08 +1200

    1st export

--- pdf-export/export.php
+++ pdf-export/export.php
@@ -468,11 +468,12 @@
 		  send_pdf_to_medimage($api, 0, $pdf_file_name, $output_folder, $layer_info['int_layer_id'], $layer_visible, $_REQUEST['sender_id'], $medimage_config);
  	   } else {
  	   	  chmod($output_folder . $pdf_file_name, "777");
+ 	   	  echo $web_path . $pdf_file_name;
  	   	  wait_and_remove_pdf($api, $output_folder . $pdf_file_name);
  	   }
  	   
  	  
- 	   echo $web_path . $pdf_file_name;
+ 	   
  	    
 	} else {
 	 //wrong username
