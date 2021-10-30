8fef05e0639e9a83dcec778b7d80c9ba92ba2af4
AtomJump <peter@atomjump.com>
2021-10-31 09:01:08 +1300

    dummy user message if not logged in

--- pdf-export/export.php
+++ pdf-export/export.php
@@ -571,7 +571,7 @@
 	 $sh = new cls_ssshout();
 	 $ip = $ly->getFakeIpAddr();  //get new user's ip address	
 			
-	 $new_user_id = $sh->new_user('', $ip, '', false);		//Should be false?
+	 $new_user_id = $sh->new_user('', $ip, '', true);		//Should be false?
 	 medimage_intro_message($api, 0, $web_path, $pdf_file_name, $layer_info['int_layer_id'], $layer_visible, $new_user_id, $medimage_config);
 	 exit(0);
 	  
